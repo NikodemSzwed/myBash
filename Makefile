0ce2f0a746afa8b58fa29eeec3798c4d6fee7fee
NickS124 <nikodem.sd@gmail.com>
2024-04-24 22:32:03 +0200

    potoki działają

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 main: main.o
 	gcc -o main main.o
+main2: main2.o
+	gcc -o main2 main2.o
 
 .PHONY: clean
 clean:
-	rm -f main *.o
+	rm -f main main2 *.o
--- main.c
+++ main.c
@@ -12,117 +12,164 @@ void clearInputBuffer() {
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void execute_command(char *command, char *args[]) {
-    if (strcmp(args[0], "cd") == 0) {
-        if (args[1] == NULL) {
-            fprintf(stderr, "cd: missing argument\n");
-        } else {
-            if (chdir(args[1]) != 0) {
-                perror("cd");
-            }
-        }
+void history(char *command){
+    FILE *file;
+    char filename[] = ".myBash_history.txt";
+    char buffer[10100]; 
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file %s\n", filename);
         return;
     }
 
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        execvp(command, args);
-        perror("execvp");
-        exit(EXIT_FAILURE);
+    fgets(buffer, sizeof(buffer), file);
+    printf("Content read from file: %s\n", buffer);
+
+  
+    fclose(file);
+
+    char *firstEnter;
+
+    firstEnter = strchr(buffer, '\n');
+    char substring[10100];
+    if (firstEnter != NULL) {
+        
+        strcpy(substring, firstEnter+1);
+        
+        printf("Substring after first newline character: %s\n", substring);
     } else {
-        int status;
-        waitpid(pid, &status, 0);
+        printf("No newline character found in the string.\n");
     }
+
+    strcat(substring, command);
+
+    file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file %s\n", filename);
+        return;
+    }
+
+    fprintf(file,"%s\n", substring);
+    fclose(file);
+
+
 }
-
-
 
 int main ()
 {
-    int fds[2];
-    pid_t pid;
-    /* Create a pipe. File descriptors for the two ends of the pipe are placed in fds. */
-    /* TODO add error handling for system calls like pipe, fork, etc. */
-    pipe (fds);
-    /* Fork a child process. */
-    pid = fork ();
-    if (pid == (pid_t) 0) {
-        /* This is the child process. Close our copy of the write end of the file descriptor. */
-        close (fds[1]);
-        /* Connect the read end of the pipe to standard input. */
-        dup2 (fds[0], STDIN_FILENO);
-        /* Replace the child process with the "sort” program. */
-        execlp ("sort", "sort", NULL);
-    } else {
-        while(1){
-            char str[1000];
-            //char delimiters[] = "|"; // Delimiters include space, comma, period, exclamation mark, and question mark
-            char *commands[100]; // Array to store words
-            int comm_count = 0;
+    while(1){
+        char str[1000];
+        char *commands[100]; 
+        int comm_count = 0;
 
-            printf("> ");
-            scanf("%999[^\n]",str);
-            clearInputBuffer();
+        printf("> ");
+        scanf("%999[^\n]",str);
+        clearInputBuffer();
+        history(str);
 
-            int pos = 0;
-            char command[100];
-            // sscanf(str, "%[^|]", command);
-            // printf("debug> %s\n", command);
-            
-            while (pos<strlen(str) && sscanf(str + pos, "%[^|]", command) == 1 && comm_count < 100) {
-                // printf("Pos: %d, Word: %s\n",pos, command);
-                commands[comm_count]=strdup(command);
-                comm_count++;
-                pos += strlen(command) + 1; 
-                while (str[pos] == '|') pos++;
-                
-            }
-            
-            for (int i=0;i<comm_count;i++){
-                char *token = strtok(commands[i], " ");
-                if (token == NULL) {
-                    continue; // Empty line
-                }
-
-                char *commando = token;
-                char *args[100];
-                args[0] = commando;
-
-                int i = 1;
-                while ((token = strtok(NULL, " ")) != NULL && i < 100) {
-                    args[i++] = token;
-                }
-                args[i] = NULL;
-                execute_command(commando, args); 
-            }
-
-
-            /*
-            printf("%d\n", comm_count);
-            for (int i = 0; i < comm_count; i++) {
-                printf("%s\n", commands[i]);
-            }
-            */
-        }
+        int pos = 0;
+        char command[100];
         
-        /* This is the parent process. */
-        FILE* stream;
-        /* Close our copy of the read end of the file descriptor. */
-        close (fds[0]);
-        /* Convert the write file descriptor to a FILE object, and write to it. */
-        stream = fdopen (fds[1], "w");
-        fprintf (stream, "This is a test.\n");
-        fprintf (stream, "Hello, world.\n");
-        fprintf (stream, "My dog has fleas.\n");
-        fprintf (stream, "This program is great.\n");
-        fprintf (stream, "One fish, two fish.\n");
-        fflush (stream);
-        close (fds[1]);
-        /* Wait for the child process to finish. */
-        waitpid (pid, NULL, 0);
+        while (pos<strlen(str) && sscanf(str + pos, "%[^|]", command) == 1 && comm_count < 100) {
+            commands[comm_count]=strdup(command);
+            comm_count++;
+            pos += strlen(command) + 1; 
+            while (str[pos] == '|') pos++;
+        }
+
+        int **pipes;
+
+        pipes = (int **)malloc((comm_count - 1) * sizeof(int *));
+        for(int i = 0; i < comm_count - 1; i++) pipes[i] = (int *)malloc(2 * sizeof(int));
+        pid_t pid;
+
+        for (int i = 0; i < comm_count - 1; i++) {
+            if (pipe(pipes[i]) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        for (int i=0;i<comm_count;i++){
+            char *token = strtok(commands[i], " ");
+            if (token == NULL) {
+                continue; // Empty line
+            }
+
+            char *commando = token;
+            char *args[100];
+            args[0] = commando;
+
+            int a = 1;
+            while ((token = strtok(NULL, " ")) != NULL && a < 100) {
+                args[a++] = token;
+            }
+            args[a] = NULL;
+
+            if (strcmp(args[0], "cd") == 0) {
+                if (args[1] == NULL) {
+                    fprintf(stderr, "cd: missing argument\n");
+                } else {
+                    if (chdir(args[1]) != 0) {
+                        perror("cd");
+                    }
+                }
+                break;
+            }
+            pid = fork();
+
+            if (pid < 0) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pid == 0) {  
+                if (i == 0) {  // First child process
+                    if(i!=comm_count-1){
+                        close(pipes[i][0]);
+                        dup2(pipes[i][1], STDOUT_FILENO);
+
+                        for (int j = 0; j < comm_count - 1; j++) {
+                            close(pipes[j][0]);
+                            close(pipes[j][1]);
+                        }
+                    }
+
+                    execvp(commando, args);
+                } else if (i == comm_count - 1) {  // Last child process
+
+                    close(pipes[i - 1][1]);
+                    dup2(pipes[i - 1][0], STDIN_FILENO);
+                    for (int j = 0; j < comm_count - 1; j++) {
+                        close(pipes[j][0]);
+                        close(pipes[j][1]);
+                    }
+
+                    execvp(commando, args);
+                } else {  // Intermediate child processes
+                    close(pipes[i - 1][1]);
+                    dup2(pipes[i - 1][0],STDIN_FILENO);
+                    close(pipes[i][0]);
+                    dup2(pipes[i][1], STDOUT_FILENO);
+
+                    for (int j = 0; j < comm_count - 1; j++) {
+                        if (j != i - 1 && j != i) {
+                            close(pipes[j][0]);
+                            close(pipes[j][1]);
+                        }
+                    }
+
+                    execvp(commando, args);
+                }
+            }
+        }
+        for (int i = 0; i < comm_count - 1; i++) {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+        for (int i = 0; i < comm_count; i++) {
+            wait(NULL);
+        }
     }
+   
     return 0;
 }
