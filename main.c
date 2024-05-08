4f1069c8165db1569d38f21827df4addfaa109fd
NickS124 <nikodem.sd@gmail.com>
2024-05-08 17:14:25 +0200

    fix for background

--- main.c
+++ main.c
@@ -9,21 +9,27 @@
 #include <ctype.h>
 #include <signal.h>
 
-void clearInputBuffer() {
+void clearInputBuffer()
+{
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
 }
 
-char *ltrim(char *str) {
-    while (isspace((unsigned char)*str)) {
+char *ltrim(char *str)
+{
+    while (isspace((unsigned char)*str))
+    {
         str++;
     }
     return str;
 }
 
-char *rtrim(char *str) {
+char *rtrim(char *str)
+{
     char *end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end)) {
+    while (end > str && isspace((unsigned char)*end))
+    {
         end--;
     }
     *(end + 1) = '\0';
@@ -32,65 +38,69 @@ char *rtrim(char *str) {
 
 char *trim(char *s)
 {
-    return rtrim(ltrim(s)); 
+    return rtrim(ltrim(s));
 }
 
-void history(char *command){
+void history(char *command)
+{
     FILE *file = fopen(".myBash_history.txt", "r+");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Error opening history file");
         return;
     }
 
     char history[20][1000];
     int lineCount = 0;
-    while (fgets(history[lineCount], 1000, file) != NULL && lineCount < 20) {
+    while (fgets(history[lineCount], 1000, file) != NULL && lineCount < 20)
+    {
         lineCount++;
     }
 
-    if (lineCount == 20) {
-        
+    if (lineCount == 20)
+    {
+
         fclose(file);
         FILE *file = fopen(".myBash_history.txt", "w+");
-        if (file == NULL) {
+        if (file == NULL)
+        {
             perror("Error opening history file");
-        return;
-    }
-        
-        for (int i=0;i<19;i++){
-            fprintf(file, "%s", history[i+1]);
+            return;
         }
 
+        for (int i = 0; i < 19; i++)
+        {
+            fprintf(file, "%s", history[i + 1]);
+        }
     }
 
-    
     fprintf(file, "%s\n", command);
     fclose(file);
 }
 
-void handle_sigquit(int sig) {
+void handle_sigquit(int sig)
+{
     printf("\nHistory:\n");
 
     FILE *file = fopen(".myBash_history.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening history file.\n");
         exit(1);
     }
 
-    
-    char line[1000]; 
-    while (fgets(line, sizeof(line), file) != NULL) {
+    char line[1000];
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
         printf("%s", line);
     }
-
 
     fclose(file);
 
     exit(0);
 }
 
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     // FILE *file;
     int file;
@@ -98,7 +108,7 @@ int main (int argc, char *argv[])
 
     signal(SIGQUIT, handle_sigquit);
 
-    if (argc==2)
+    if (argc == 2)
     {
         script = 1;
         /*file = fopen(argv[1], "r");
@@ -111,24 +121,28 @@ int main (int argc, char *argv[])
         getc();*/
         // fscanf(file,"%999[^\n]",buffer);
 
-        file = open(argv[1],O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-        if (file == -1) {
+        file = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (file == -1)
+        {
             perror("open");
             exit(EXIT_FAILURE);
         }
         dup2(file, STDIN_FILENO);
     }
     int firstLine = 1;
-    
-    while(1){
+    pid_t pid = 0;
+
+    while (1)
+    {
         char str[1000] = "";
-        char *commands[100]; 
-        int outputs[100]; 
+        char *commands[100];
+        int outputs[100];
         int comm_count = 0;
         int pos = 0;
         char command[100] = "";
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             outputs[i] = 0;
             commands[i] = NULL;
         }
@@ -136,97 +150,81 @@ int main (int argc, char *argv[])
         int help = 0;
         printf("> ");
 
-        /*if(script){
-            /*while (fscanf(file, "%s", str) == 1) {
-                printf("%s\n", str); // Print the value read from the file
-            }*/
-            //fscanf(file,"%999[^\n]",str);
-            //printf("1>%s\n", str);
-            // if(fscanf(file, "%999[^\r]", str) != 1) exit(EXIT_SUCCESS);
+        if (script && firstLine)
+        {
+            help = scanf("%999[^\n]", str);
+            firstLine = 0;
+        }
 
-            // printf("2>%s\n", str);//*/
-            //if(feof(file)) exit(EXIT_SUCCESS);
-            /*char *line;
-            size_t bufsize = 0;
+        help = scanf("%999[^\n]", str);
+        
+        int status;
+        if(pid>0)waitpid(pid, &status, WNOHANG);
+        if(status)printf("\n> ");
+            
+        // printf("%s\n", str);
 
-            // Read a line from the file
-            if (getline(&line, &bufsize, file) == -1) {
-                fprintf(stderr, "Error reading input\n");
-                exit(EXIT_FAILURE);
-            }
-            
-
-            printf("%s\n", str);
-            clearInputBuffer();*/
-            // Remove newline character, if present
-            /*char *newline = strchr(line, '\n');
-            if (newline != NULL) {
-                *newline = '\0';
-            }*/
-        /*} 
-        else {*/
-            if (script&&firstLine)
-            {
-                help = scanf("%999[^\n]",str);
-                firstLine = 0;
-            }
-            
-            help = scanf("%999[^\n]",str);
-            printf("%s\n", str);
-            
-            if (help==EOF&&script)
-            {
-                exit(EXIT_SUCCESS);
-            }
-            clearInputBuffer();
-            if(help!=1) continue;
+        if (help == EOF && script)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        clearInputBuffer();
+        if (help != 1)
+            continue;
         // }
 
-        
         history(str);
-        
-        while (pos<strlen(str) && sscanf(str + pos, "%[^|]", command) == 1 && comm_count < 100) {
-            commands[comm_count]=strdup(command);
+
+        while (pos < strlen(str) && sscanf(str + pos, "%[^|]", command) == 1 && comm_count < 100)
+        {
+            commands[comm_count] = strdup(command);
             comm_count++;
-            pos += strlen(command) + 1; 
-            while (str[pos] == '|') pos++;
+            pos += strlen(command) + 1;
+            while (str[pos] == '|')
+                pos++;
         }
 
         int background = 0;
-        char last = commands[comm_count-1][strlen(commands[comm_count-1])-1];
-        if(last == '&'){
+        char last = commands[comm_count - 1][strlen(commands[comm_count - 1]) - 1];
+        if (last == '&')
+        {
             background = 1;
-            commands[comm_count-1][strlen(commands[comm_count-1])-1] = '\0';
+            commands[comm_count - 1][strlen(commands[comm_count - 1]) - 1] = '\0';
         }
 
         int **pipes;
 
         pipes = (int **)malloc((comm_count - 1) * sizeof(int *));
-        for(int i = 0; i < comm_count - 1; i++) pipes[i] = (int *)malloc(2 * sizeof(int));
-        pid_t pid;
+        for (int i = 0; i < comm_count - 1; i++)
+            pipes[i] = (int *)malloc(2 * sizeof(int));
 
-        for (int i = 0; i < comm_count - 1; i++) {
-            if (pipe(pipes[i]) == -1) {
+        for (int i = 0; i < comm_count - 1; i++)
+        {
+            if (pipe(pipes[i]) == -1)
+            {
                 perror("pipe");
                 exit(EXIT_FAILURE);
             }
         }
 
-        for (int i=0;i<comm_count;i++){
+        for (int i = 0; i < comm_count; i++)
+        {
             char *tempCommand = strtok(commands[i], ">>");
             char *outputFile;
             if (tempCommand != NULL)
             {
                 outputFile = strtok(NULL, ">>");
 
-                if (outputFile != NULL) {
+                if (outputFile != NULL)
+                {
                     outputFile = trim(outputFile);
                     outputs[i] = 1;
                 }
             }
-            
+
             char *token = strtok(tempCommand, " ");
-            if (token == NULL) {
+            if (token == NULL)
+            {
                 continue; // Empty line
             }
 
@@ -235,16 +233,22 @@ int main (int argc, char *argv[])
             args[0] = commando;
 
             int a = 1;
-            while ((token = strtok(NULL, " ")) != NULL && a < 100) {
+            while ((token = strtok(NULL, " ")) != NULL && a < 100)
+            {
                 args[a++] = token;
             }
             args[a] = NULL;
 
-            if (strcmp(args[0], "cd") == 0) {
-                if (args[1] == NULL) {
+            if (strcmp(args[0], "cd") == 0)
+            {
+                if (args[1] == NULL)
+                {
                     fprintf(stderr, "cd: missing argument\n");
-                } else {
-                    if (chdir(args[1]) != 0) {
+                }
+                else
+                {
+                    if (chdir(args[1]) != 0)
+                    {
                         perror("cd");
                     }
                 }
@@ -252,54 +256,77 @@ int main (int argc, char *argv[])
             }
             pid = fork();
 
-            if (pid < 0) {
+            if (pid < 0)
+            {
                 perror("fork");
                 exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                if(outputs[i]){
-                    int file_fd = open(outputFile,O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-                    if (file_fd == -1) {
+            }
+            else if (pid == 0)
+            {
+                if (outputs[i])
+                {
+                    int file_fd = open(outputFile, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                    if (file_fd == -1)
+                    {
                         perror("open");
                         exit(EXIT_FAILURE);
                     }
                     dup2(file_fd, STDOUT_FILENO);
                 }
 
-                if (i == 0) {  // First child process
-                    if(i!=comm_count-1){
+                if (i == 0)
+                { // First child process
+                    if (i != comm_count - 1)
+                    {
                         close(pipes[i][0]);
-                        if(outputs[i]) close(pipes[i][1]);
-                        else dup2(pipes[i][1], STDOUT_FILENO);
+                        if (outputs[i])
+                            close(pipes[i][1]);
+                        else
+                            dup2(pipes[i][1], STDOUT_FILENO);
 
-                        for (int j = 0; j < comm_count - 1; j++) {
+                        for (int j = 0; j < comm_count - 1; j++)
+                        {
                             close(pipes[j][0]);
                             close(pipes[j][1]);
                         }
                     }
 
                     execvp(commando, args);
-                } else if (i == comm_count - 1) {  // Last child process
+                }
+                else if (i == comm_count - 1)
+                { // Last child process
                     close(pipes[i - 1][1]);
-                    if(outputs[i-1]) close(pipes[i-1][0]);
-                    else dup2(pipes[i - 1][0], STDIN_FILENO);
+                    if (outputs[i - 1])
+                        close(pipes[i - 1][0]);
+                    else
+                        dup2(pipes[i - 1][0], STDIN_FILENO);
 
-                    for (int j = 0; j < comm_count - 1; j++) {
+                    for (int j = 0; j < comm_count - 1; j++)
+                    {
                         close(pipes[j][0]);
                         close(pipes[j][1]);
                     }
 
                     execvp(commando, args);
-                } else {  // Intermediate child processes
+                }
+                else
+                { // Intermediate child processes
                     close(pipes[i - 1][1]);
-                    if(outputs[i-1]) close(pipes[i-1][0]);
-                    else dup2(pipes[i - 1][0], STDIN_FILENO);
-                    
-                    close(pipes[i][0]);
-                    if(outputs[i]) close(pipes[i][1]);
-                    else dup2(pipes[i][1], STDOUT_FILENO);
+                    if (outputs[i - 1])
+                        close(pipes[i - 1][0]);
+                    else
+                        dup2(pipes[i - 1][0], STDIN_FILENO);
 
-                    for (int j = 0; j < comm_count - 1; j++) {
-                        if (j != i - 1 && j != i) {
+                    close(pipes[i][0]);
+                    if (outputs[i])
+                        close(pipes[i][1]);
+                    else
+                        dup2(pipes[i][1], STDOUT_FILENO);
+
+                    for (int j = 0; j < comm_count - 1; j++)
+                    {
+                        if (j != i - 1 && j != i)
+                        {
                             close(pipes[j][0]);
                             close(pipes[j][1]);
                         }
@@ -310,19 +337,23 @@ int main (int argc, char *argv[])
                 exit(EXIT_SUCCESS);
             }
         }
-        for (int i = 0; i < comm_count - 1; i++) {
+        for (int i = 0; i < comm_count - 1; i++)
+        {
             close(pipes[i][0]);
             close(pipes[i][1]);
         }
-        if(background==0){
-            for (int i = 0; i < comm_count; i++) {
+        if (background == 0)
+        {
+            for (int i = 0; i < comm_count; i++)
+            {
                 wait(NULL);
             }
-            
-        }else{
+        }
+        else
+        {
             background = 0;
         }
     }
-   
+
     return 0;
 }
